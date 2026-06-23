20453d2f0563c5380508808cbea23a87896d6eae
pramesh shrestha <prameshshrestha455@gmail.com>
2026-06-23 10:57:42 +0545

    add prod keys

--- app/Http/Controllers/Api/EsewaPaymentController.php
+++ app/Http/Controllers/Api/EsewaPaymentController.php
@@ -115,11 +115,12 @@ class EsewaPaymentController extends Controller
                 'message' => 'Customer not found.'
             ], 422);
         }
-        $secret = "8gBm/:&EnhH.1/q";
+        // $secret = "8gBm/:&EnhH.1/q";
+        $secret = "LhIRHwwSBQEMRSAMEAkHGAAcDB0CVzEFH0tZKQcBWVs9O0g8Nl42PiY7PzY8IDorMFs2OyQgOjImNCQgODoyICo";  //production secret key
 
         $transaction_uuid = uniqid();
 
-        $data = "total_amount={$booking->total_amount},transaction_uuid={$transaction_uuid},product_code=EPAYTEST";
+        $data = "total_amount={$booking->total_amount},transaction_uuid={$transaction_uuid},product_code=NP-ES-SIGHTSEEING";
 
 
         // Create payment record
